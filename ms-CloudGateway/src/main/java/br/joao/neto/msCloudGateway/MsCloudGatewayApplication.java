f187abd57fca77a4b478beb142ca3530435bf314
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-10-16 14:51:23 -0300

    feat/cloudGateway implementation

---
+++ ms-CloudGateway/src/main/java/br/joao/neto/msCloudGateway/MsCloudGatewayApplication.java
@@ -0,0 +1,23 @@
+package br.joao.neto.msCloudGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsCloudGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsCloudGatewayApplication.class, args);
+	}
+
+	/*@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder.routes()
+                .route("ms-cloud-gateway", r -> r.path("/ms-cloud-gateway/**").uri("lb://ms-cloud-gateway"))
+                .build();
+	}*/
+}
---
+++ ms-CloudGateway/src/test/java/br/joao/neto/msCloudGateway/MsCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.joao.neto.msCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
