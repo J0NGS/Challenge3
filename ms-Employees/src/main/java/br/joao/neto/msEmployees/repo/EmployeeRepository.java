0dbabfaa2975dc762c13453b575c8f4f2369c567
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-10-19 19:08:19 -0300

    refactor/ refactor employee

--- ms-Employees/src/main/java/br/joao/neto/msEmployees/repo/EmployeeRepository.java
+++ ms-Employees/src/main/java/br/joao/neto/msEmployees/repo/EmployeeRepository.java
@@ -1,13 +1,19 @@
 package br.joao.neto.msEmployees.repo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.joao.neto.msEmployees.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, UUID>{
+    boolean existsByCpf(String cpf);
     Optional<Employee> findBycpf(String cpf);
-    Optional<Employee> findByNameContainingIgnoreCase(String name);
+    Optional<List<Employee>> findByNameIgnoreCase(String name);
+    Optional<List<Employee>> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<Employee> findAll(Pageable pageable);
 }
--- ms-Employees/src/main/java/br/joao/neto/msEmployees/service/EmployeeService.java
+++ ms-Employees/src/main/java/br/joao/neto/msEmployees/service/EmployeeService.java
@@ -1,15 +1,22 @@
 package br.joao.neto.msEmployees.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.joao.neto.msEmployees.model.Employee;
+import br.joao.neto.msEmployees.model.DTO.EmployeeGetResponse;
 import br.joao.neto.msEmployees.repo.EmployeeRepository;
 
 @Service
@@ -22,40 +29,62 @@ public class EmployeeService {
     }
 
     public ResponseEntity<Employee> create(Employee employee){
+        if(repository.existsByCpf(employee.getCpf()))
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "CPF already exists");
         return new ResponseEntity<>(repository.save(employee), HttpStatus.CREATED);
     }
 
     public ResponseEntity<Employee> findById(UUID id){
         Optional<Employee> employee = repository.findById(id);
-        if(employee.isPresent())
-            return new ResponseEntity<>(repository.findById(id).get(), HttpStatus.FOUND);
-        else
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "empolyee with id " + id + " not found");
+        return employee.map(emp -> new ResponseEntity<>(emp, HttpStatus.FOUND))
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "employee with id [ " + id + " ] not found"));
     }
 
-    public ResponseEntity<Employee> findByCpf(String cpf){
+    public ResponseEntity<EmployeeGetResponse> findByCpf(String cpf){
         Optional<Employee> employee = repository.findBycpf(cpf);
-        if(employee.isPresent())
-            return new ResponseEntity<>(repository.findBycpf(cpf).get(), HttpStatus.FOUND);
-        else
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "empolyee with cpf " + cpf + " not found");
+        EmployeeGetResponse response = new EmployeeGetResponse();
+        return employee.map(emp -> new ResponseEntity<>(response.toModel(emp), HttpStatus.FOUND))
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "employee with cpf [ " + cpf + " ] not found"));
     }
 
-    public ResponseEntity<Employee> findByName(String name){
-        Optional<Employee> employee = repository.findByNameContainingIgnoreCase(name);
-        if(employee.isPresent())
-            return new ResponseEntity<>(repository.findByNameContainingIgnoreCase(name).get(), HttpStatus.FOUND);
-        else
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "empolyee with name " + name + " not found");
+    public ResponseEntity<List<EmployeeGetResponse>> findByName(String name){
+        Optional<List<Employee>> employee = repository.findByNameIgnoreCase(name);
+        EmployeeGetResponse response = new EmployeeGetResponse();
+        if (employee.isPresent()){
+            List<EmployeeGetResponse> list = new ArrayList<>();
+            for (Employee emp : employee.get()) {
+                list.add(response.toModel(emp));
+            }
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "employee with name [ " + name + " ] not found");
+        }
     }
 
-    public ResponseEntity<Employee> update(Employee employee){
-        Employee updateEmployee = new Employee();
-        updateEmployee.setId(employee.getId());
-        if(!employee.getName().isBlank())
-            updateEmployee.setName(employee.getName());
-        else
-            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "empolyee name is empty");
-        return new ResponseEntity<>(repository.save(employee), HttpStatus.OK);
+    public ResponseEntity<List<Employee>> findAll(int page, int quantity){
+        Pageable pageable = PageRequest.of(page, quantity);
+        Page<Employee> response = repository.findAll(pageable);
+
+        List<Employee> list = new ArrayList<>();
+
+        list = response.getContent();
+
+        if(response.isEmpty())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Page is empty");
+        else 
+            return new ResponseEntity<>(list, HttpStatus.OK);
+    }   
+
+
+    public ResponseEntity<EmployeeGetResponse> update(UUID id, String name){
+        Employee updateEmployee = findById(id).getBody();
+        if (!name.isBlank()) {
+            updateEmployee.setName(name);
+            repository.save(updateEmployee);
+            EmployeeGetResponse response = new EmployeeGetResponse();
+            return new ResponseEntity<>(response.toModel(updateEmployee), HttpStatus.OK);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "name is empty");
+        }
     }
 }
