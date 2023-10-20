e006ca0535fe08969530c75e338ad13bb95ba85b
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-10-20 16:11:04 -0300

    feat/implement controller VotingSession

---
+++ ms-VotingSession/src/main/java/br/joao/neto/msVotingSession/controller/VotesController.java
@@ -0,0 +1,44 @@
+package br.joao.neto.msVotingSession.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.joao.neto.msVotingSession.model.Votes;
+import br.joao.neto.msVotingSession.service.VotesService;
+
+@RestController
+@RequestMapping("/votes")
+public class VotesController {
+
+    private VotesService votesService;
+
+    @Autowired
+    public VotesController(VotesService votesService) {
+        this.votesService = votesService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Votes> createVote(@RequestBody Votes vote) {
+        return votesService.createVote(vote);
+    }
+
+    @GetMapping("/voting-session/{votingSessionId}")
+    public ResponseEntity<Page<Votes>> getVotesByVotingSessionId(
+        @PathVariable UUID votingSessionId,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        return votesService.getVotesByVotingSessionId(votingSessionId, PageRequest.of(page, size));
+    }
+}
---
+++ ms-VotingSession/src/main/java/br/joao/neto/msVotingSession/controller/VotingSessionController.java
@@ -0,0 +1,44 @@
+package br.joao.neto.msVotingSession.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.joao.neto.msVotingSession.model.VotingSession;
+import br.joao.neto.msVotingSession.service.VotingSessionService;
+
+@RestController
+@RequestMapping("/votingSession")
+public class VotingSessionController {
+
+    private VotingSessionService votingSessionService;
+
+    @Autowired
+    public VotingSessionController(VotingSessionService votingSessionService) {
+        this.votingSessionService = votingSessionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<VotingSession> createVotingSession(@RequestBody VotingSession votingSession) {
+        return votingSessionService.createVotingSession(votingSession);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VotingSession> getVotingSessionById(@PathVariable UUID id) {
+        return votingSessionService.getVotingSessionById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<VotingSession>> getAllVotingSessions(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        return votingSessionService.getAllVotingSessions(page, size);
+    }
+}
